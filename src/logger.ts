da8c6808742b309c4bd47e74902baf80e59f3518
takumi-n <takumi.chillout@gmail.com>
2022-04-22 23:22:06 +0900

    feat: add logger

---
+++ src/logger.ts
@@ -0,0 +1,27 @@
+import picocolors from 'picocolors';
+
+type LogLevel = 'error' | 'warn' | 'info';
+
+export function log(level: LogLevel, message: string) {
+  const bold = picocolors.bold(` ${level} `);
+  const prefix =
+    level === 'error'
+      ? picocolors.bgRed(bold)
+      : level === 'warn'
+      ? picocolors.bgYellow(bold)
+      : picocolors.bgCyan(bold);
+
+  console.log(`${prefix} ${message}`);
+}
+
+export function info(message: string) {
+  log('info', message);
+}
+
+export function warn(message: string) {
+  log('warn', message);
+}
+
+export function error(message: string) {
+  log('error', message);
+}
