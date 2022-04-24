bf429745860866003f2c05fc78fe5c5fa9be3deb
takumi-n <takumi.chillout@gmail.com>
2022-04-25 00:51:53 +0900

    wip index.ts

--- index.ts
+++
@@ -1,3 +0,0 @@
-import picocolors from "picocolors";
-
-console.log(`${picocolors.bgGreen(picocolors.bold('info'))} Hello world`);
---
+++ src/index.ts
@@ -0,0 +1,60 @@
+import { cac } from 'cac';
+import fs from 'fs';
+import { execYarnAudit } from './exec';
+import { error } from './logger';
+import { Audit, convertAuditObj, normalizeAudits } from './audit';
+import pkg from '../package.json';
+
+async function main() {
+  const cli = cac(pkg.name);
+  cli.option('-f, --file <file>', 'File to read from');
+  cli.version(pkg.version);
+  cli.help();
+  const parsed = cli.parse();
+
+  const audits: Audit[] = [];
+
+  if (parsed.options.file) {
+    if (
+      (typeof parsed.options.file !== 'string' &&
+        typeof parsed.options.file !== 'number') ||
+      `${parsed.options.file}` === ''
+    ) {
+      error('File must be specified');
+      process.exit(1);
+    }
+
+    const file = `${parsed.options.file}`;
+    if (!fs.existsSync(file)) {
+      error(`File ${file} does not exist`);
+      process.exit(1);
+    }
+
+    const fileContents = fs.readFileSync(file);
+    audits.push(
+      ...JSON.parse(fileContents.toString())
+        .filter((obj: any) => obj.type === 'auditAdvisory') // eslint-disable-line @typescript-eslint/no-explicit-any
+        .map(convertAuditObj)
+    );
+  } else {
+    for await (const obj of execYarnAudit()) {
+      if (obj.type !== 'auditAdvisory') {
+        continue;
+      }
+
+      audits.push(convertAuditObj(obj));
+    }
+  }
+
+  const normalized = normalizeAudits(audits);
+  console.log(JSON.stringify(normalized, null, 2));
+}
+
+main();
+
+// const npm = new NpmApi();
+
+// npm
+//   .repo('handsontable')
+//   .dependencies()
+//   .then((packages: any) => console.log(packages));
